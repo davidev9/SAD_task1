72323c77ccbc0b32afe47685d6bfc9f70885df2a
davidev9 <101256668+davidev9@users.noreply.github.com>
2023-05-13 17:43:25 +0200

    Delete SAD_task1/manvsclass directory

--- SAD_task1/manvsclass/src/main/java/com/groom/manvsclass/repository/ClassRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groom.manvsclass.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.groom.manvsclass.model.ClassUT;
-
-public interface ClassRepository	extends MongoRepository<ClassUT,String>{
-
-}
