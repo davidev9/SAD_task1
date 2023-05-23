eb2cd324322337c518c4c3363013bfbc60d4cfae
davidev9 <101256668+davidev9@users.noreply.github.com>
2023-05-23 12:56:51 +0200

    Add files via upload

--- manvsclass/src/main/java/com/groom/manvsclass/controller/PostController.java
+++ manvsclass/src/main/java/com/groom/manvsclass/controller/PostController.java
@@ -2,23 +2,21 @@ package com.groom.manvsclass.controller;
 
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.io.File;
 import java.time.LocalDate;
 import java.util.List;
-import org.springframework.data.mongodb.core.query.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,13 +30,12 @@ import com.groom.manvsclass.repository.SearchRepositoryImpl;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+//@Controller
 @RestController
 public class PostController {
 	
 	@Autowired
 	ClassRepository repo;
-	@Autowired
-    private MongoTemplate mongoTemplate; 
 	
 	private final LocalDate today = LocalDate.now();
 	private final SearchRepositoryImpl srepo;
@@ -47,33 +44,23 @@ public class PostController {
 	{
 		this.srepo=srepo;
 	}
-
+	
 	@GetMapping("/home")
-	public	List<ClassUT>	estraiClassi()
+	public	List<ClassUT>	elencaClassi()
+	{
+		return repo.findAll();
+	}
+	
+	@GetMapping("/classut")
+	public	List<ClassUT>	elenscaClassi()
 	{
 		return repo.findAll();
 	}
 
-	@GetMapping("filterby/{category}")
-	public List<ClassUT> estraiClassi(@PathVariable String category)
-	{
-		return srepo.filterByCategory(category);
-	}
-
-	@GetMapping("/filterby/{text}/{category}")
-	public	List<ClassUT>	estraiClassi(@PathVariable String text,@PathVariable String category)
-	{
-		return srepo.searchAndFilter(text,category);
-	}
-
-	@PostMapping("/insert")
-	public ClassUT UploadClasse(@RequestBody ClassUT classe)
-	{
-		return repo.save(classe);
-	}
-
+	
+	
 	@PostMapping("/uploadFile")
-	public ResponseEntity<FileUploadResponse> uploadFile(@RequestParam("file") MultipartFile multipartFile,@RequestParam("model") String model) throws IOException
+	public ResponseEntity<FileUploadResponse> aggiuntaClasse(@RequestParam("file") MultipartFile multipartFile,@RequestParam("model") String model) throws IOException
 	{
 		ObjectMapper mapper = new ObjectMapper();
 		ClassUT classe = mapper.readValue(model, ClassUT.class);
@@ -90,55 +77,12 @@ public class PostController {
 		
 		classe.setUri("Files-Upload/"+classe.getName()+"/"+fileName);
 		classe.setDate(today.toString());
+		// generaTest_Randoop("Files-Upload/"+classe.getName()+"/"+fileName);
+		// generaTest_Evosuite("Files-Upload/"+classe.getName()+"/"+fileName);
 		repo.save(classe);
 		return new ResponseEntity<>(response,HttpStatus.OK);
 	}
 	
-	@PostMapping("delete/{name}")
-	public ClassUT eliminaClasse(@PathVariable String name) {
-		Query query= new Query(); 
-	   query.addCriteria(Criteria.where("name").is(name));
-	   return mongoTemplate.findAndRemove(query, ClassUT.class);
-	}
-
-	@PostMapping("/deleteFile/{fileName}")
-	public ResponseEntity<String> eliminaFile(@PathVariable String fileName) {
-	  String folderPath = "Files-Upload/"+ fileName; 
-	  
-	        File folderToDelete = new File(folderPath);
-	        if (folderToDelete.exists() && folderToDelete.isDirectory()) {
-	        	try {
-	                deleteDirectory(folderToDelete);
-	                return new ResponseEntity<>("Cartella eliminata con successo.", HttpStatus.OK);
-	            } catch (IOException e) {
-	                return new ResponseEntity<>("Impossibile eliminare la cartella.", HttpStatus.INTERNAL_SERVER_ERROR);
-	            }
-	        } else {
-	            return new ResponseEntity<>("Cartella non trovata.", HttpStatus.NOT_FOUND);
-	        }
-	 }
-	//Da spostare    
-	private void deleteDirectory(File directory) throws IOException {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    deleteDirectory(file);
-                } else {
-                    if (!file.delete()) {
-                        throw new IOException("Impossibile eliminare il file: " + file.getAbsolutePath());
-                    }
-                }
-            }
-        }
-        else {
-        	directory.delete();
-        }
-        if (!directory.delete()) {
-            throw new IOException("Impossibile eliminare la cartella: " + directory.getAbsolutePath());
-        }
-    }
-
 	
 	@GetMapping("/home/{text}")
 	public	List<ClassUT>	ricercaClasse(@PathVariable String text)
@@ -147,32 +91,39 @@ public class PostController {
 	}
 	
 	
-	@GetMapping("/downloadFile/{name}")
+	
+	@PostMapping("/insert")
+	public ClassUT UploadClasse(@RequestBody ClassUT classe)
+	{
+		return repo.save(classe);
+	}
+	
+	
+	
+	@GetMapping("/filterby/{text}/{category}")
+	public	List<ClassUT>	elencaClasse(@PathVariable String text,@PathVariable String category)
+	{
+		return srepo.searchAndFilter(text,category);
+	}
+
+	@GetMapping("filterby/{category}")
+	public List<ClassUT> elencaClasse(@PathVariable String category)
+	{
+		return srepo.filterByCategory(category);
+	}
+	
+	 @GetMapping("/downloadFile/{name}")
 	    public ResponseEntity<?> downloadClasse(@PathVariable("name") String name) throws Exception {
 		 	   List<ClassUT> classe= srepo.findByText(name);
 	           return FileDownloadUtil.downloadClassFile(classe.get(0).getcode_Uri());
 	    }
 	 
-
-@PostMapping("update/{name}")
-public ResponseEntity<String> modificaClasse(@PathVariable String name, @RequestBody ClassUT newContent) {
-			Query query= new Query();
-			
-		   query.addCriteria(Criteria.where("name").is(name));
-		    Update update = new Update().set("name", newContent.getName())
-	                .set("date", newContent.getDate())
-	                .set("difficulty", newContent.getDifficulty())
-	                .set("description", newContent.getDescription())
-	                .set("category", newContent.getCategory());
-		    long modifiedCount = mongoTemplate.updateFirst(query, update, ClassUT.class).getModifiedCount();
-
-	        if (modifiedCount > 0) {
-	            return new ResponseEntity<>("Aggiornamento eseguito correttamente.", HttpStatus.OK);
-	        } else {
-	            return new ResponseEntity<>("Nessuna classe trovata o nessuna modifica effettuata.", HttpStatus.NOT_FOUND);
-	        }
-	    } 
-
+	 
+	
+	     
+	
+	 
+	
 	/*
 	@PostMapping("/uploadFile")
 	public ResponseEntity<FileUploadResponse> uploadFile(@RequestParam("file") MultipartFile multipartFile) throws IOException
