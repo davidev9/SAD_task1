4af60f8d8582f273dec2af2208b9b21c715b8b08
davidev9 <101256668+davidev9@users.noreply.github.com>
2023-07-11 10:17:24 +0200

    Delete manvsclass/src directory

--- manvsclass/src/main/java/com/groom/manvsclass/repository/InteractionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groom.manvsclass.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.groom.manvsclass.model.interaction;
-
-public interface InteractionRepository	extends MongoRepository<interaction,String>{
-
-}
